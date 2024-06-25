7d086078b73698f6d6c192908be2149601778e4b
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-06-25 14:41:50 -0230

    Added the ability to add/remove/edit any publication

--- LibraryItems/BookElectronic.java
+++ LibraryItems/BookElectronic.java
@@ -4,8 +4,8 @@ import Authors.Author;
 public class BookElectronic extends Book {
   private int numOfPages;
 
-  public BookElectronic(String title, Author author, String publisher, String ISBN, int numOfPages) {
-    super(title, author, publisher, ISBN);
+  public BookElectronic(String title, Author author, String publisher, int publicationId, String ISBN, int numOfPages) {
+    super(title, author, publisher, publicationId, ISBN);
     this.numOfPages = numOfPages;
   }
 
@@ -15,5 +15,13 @@ public class BookElectronic extends Book {
 
   public void setNumOfPages(int numOfPages) {
     this.numOfPages = numOfPages;
+  }
+
+  public static void editPublication(BookElectronic book, String newTitle, Author newAuthor, String newPublisher, String newISBN, int newNumOfPages) {
+    book.setTitle(newTitle);
+    book.setAuthor(newAuthor);
+    book.setPublisher(newPublisher);
+    book.setISBN(newISBN);
+    book.setNumOfPages(newNumOfPages);
   }
 }
